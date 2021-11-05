0f0cdf527d52dafaabe9c74729cb77f2744d0e66
jiamingming <jiamingming@jd.com>
2021-11-05 19:10:01 +0800

    pwned

--- verifier/pwned.go
+++ verifier/pwned.go
@@ -27,19 +27,17 @@ var (
 	ErrNonHTTPOk   = errors.New("Non HTTP OK in API Call")
 )
 
-// PwnedVerify
+// PwnedVerify 原说明如下
 // In order to protect the value of the source password being searched for,
 // Pwned Passwords also implements a k-Anonymity model that allows a password
 // to be searched for by partial hash. This allows the first 5 characters
 // of a SHA-1 password hash (not case-sensitive) to be passed to the API (testable by clicking here):
 // GET https://api.pwnedpasswords.com/range/{first 5 hash chars}
 func (mpe *MPasswordEval) PwnedVerify(pwd string) (bool, string, error) {
-	// error if rune count is 0 (or less ?)
 	if utf8.RuneCountInString(pwd) <= 0 {
 		return false, "", ErrStringEmpty
 	}
 
-	// construct a new http.Client if not already supplied
 	if mpe.HTTPClient == nil {
 		mpe.HTTPClient = &http.Client{
 			Timeout: 5 * time.Second,
@@ -50,19 +48,18 @@ func (mpe *MPasswordEval) PwnedVerify(pwd string) (bool, string, error) {
 	data := []byte(pwd)
 	hash := sha1.Sum(data)
 
-	// get prefix suffix
+	// get prefix and suffix
 	hex := strings.ToUpper(hex.EncodeToString(hash[0:]))
 	prefix := hex[0:5]
 	suffix := hex[5:]
-
-	// hit the API
+	// request url
 	fullURL := baseURL + prefix
 	req, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
 		return false, "", errors.Wrap(err, "fail in creating new http request")
 	}
 
-	// be a friendly client and tell the API who are you
+	// set user agent
 	req.Header.Set("User-Agent", "github.com/jiamingming/mpasswordeval")
 
 	resp, err := mpe.HTTPClient.Do(req)
@@ -80,18 +77,15 @@ func (mpe *MPasswordEval) PwnedVerify(pwd string) (bool, string, error) {
 		return false, "", errors.Wrap(err, "fail in reading response body")
 	}
 
-	//fmt.Println("the pwned response: ", string(body))
-	// check suffix in response body
+	// response
 	lines := strings.Split(string(body), "\n")
 	for _, line := range lines {
 		tokens := strings.Split(line, ":")
-		// if it matches with the API response, return as not safe
 		if suffix == tokens[0] {
 			occurence := string(tokens[1])
 			return false, fmt.Sprintf("shows in pwnedpasswords with occurence: %s", occurence), nil
 		}
 	}
 
-	// if the suffix doesn't match with any API response data, return as safe
 	return true, "", nil
 }
